cf187862a11b8e00b3052a3459357ffb03ecbe67
nehrman <nicolas.ehrman@gmail.com>
2019-12-04 05:18:18 -0800

    Adding .gitignore file

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
-variable "base_worskpace_variables" {
+variable "base_workspace_variables" {
   description = "Define list of variables needed for the Base_Layer Workspace"
   type = list
   default = []
 }
 
-variable "instance_worskpace_variables" {
+variable "instance_workspace_variables" {
   description = "Define list of variables needed for the Instance Workspace"
   type = list
   default = []
