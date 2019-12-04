166e4f1bb91647eb38706054d703d75f5e7c8879
nehrman <nicolas.ehrman@gmail.com>
2019-12-04 05:07:28 -0800

    Adding variables.tf file

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variables "base_worskpace_variables" {
+  description = "Define list of variables needed for the Base_Layer Workspace"
+  type = list
+  default = []
+}
+
+variables "instance_worskpace_variables" {
+  description = "Define list of variables needed for the Instance Workspace"
+  type = list
+  default = []
+}
